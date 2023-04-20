b96167a632305eaf25bcc1a426c38cad576b94f7
ikenna-nwobodo <nikenna06@gmail.com>
2023-04-20 11:13:15 +0100

    upload commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var themeToggler = document.querySelector(".theme-toggler");
+
+themeToggler.addEventListener("click", () => {
+  console.log("fdkhsj");
+  document.body.classList.toggle("dark-variables");
+});
